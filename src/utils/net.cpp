780c59b25ba8ef3afb26b64625c0d5283f9e7a50
Folkert van Heusden <folkert@vanheusden.com>
2026-08-02 17:02:59 +0200

    get_uintXX: fix

--- src/utils/net.cpp
+++ src/utils/net.cpp
@@ -1,11 +1,13 @@
 #include <cstdint>
+#include <arpa/inet.h>
+
 
 uint16_t get_uint16(const uint8_t from[2])
 {
-	return ntohs(*reinterpret_cast<uint16_t>(from));
+	return ntohs(*reinterpret_cast<const uint16_t *>(from));
 }
 
 uint32_t get_uint32(const uint8_t from[4])
 {
-	return ntohl(*reinterpret_cast<uint32_t>(from));
+	return ntohl(*reinterpret_cast<const uint32_t *>(from));
 }
