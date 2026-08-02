39925517349ceee1ffb8cb7bc67eecb61a43fdef
Folkert van Heusden <folkert@vanheusden.com>
2026-08-02 21:01:09 +0200

    get_uintXX -> handle non-aligned access

--- src/utils/net.cpp
+++ src/utils/net.cpp
@@ -1,13 +1,12 @@
 #include <cstdint>
-#include <arpa/inet.h>
 
 
 uint16_t get_uint16(const uint8_t from[2])
 {
-	return ntohs(*reinterpret_cast<const uint16_t *>(from));
+	return (from[0] << 8) | from[1];
 }
 
 uint32_t get_uint32(const uint8_t from[4])
 {
-	return ntohl(*reinterpret_cast<const uint32_t *>(from));
+	return (from[0] << 24) | (from[1] << 16) | (from[2] << 8) | from[3];
 }
