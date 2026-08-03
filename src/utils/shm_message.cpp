52d906e2f90d6e7ab57678285f87f4b2c40da3e4
Folkert van Heusden <folkert@vanheusden.com>
2026-08-03 11:07:23 +0200

    split (un-)wrap in raw/shm_message_queue::message versions

--- src/utils/shm_message.cpp
+++ src/utils/shm_message.cpp
@@ -5,24 +5,13 @@
 #include "shm.h"
 
 
-shm_message_queue::message * wrap_message(const size_t from_len, const uint8_t *const from,
+std::pair<uint8_t *, size_t> wrap_message(const size_t from_len, const uint8_t *const from,
                                           const size_t to_len,   const uint8_t *const to,
-                                          const size_t pl_len,   const uint8_t *const pl,
-					  const std::optional<uint64_t> reply_to)
+                                          const size_t pl_len,   const uint8_t *const pl)
 {
 	size_t   total_pl_length = 2 + from_len + 2 + to_len + 4 + pl_len;
-	shm_message_queue::message *msg = allocate_shm_message(total_pl_length);
-	uint8_t *payload         = reinterpret_cast<uint8_t *>(msg->data);
+	uint8_t *payload         = reinterpret_cast<uint8_t *>(malloc(total_pl_length));
 	size_t   offset          = 0;
-
-	msg->size = total_pl_length;
-	if (reply_to.has_value()) {
-		msg->msg_nr = reply_to.value();
-		msg->type   = shm_message_queue::msg_reply;
-	}
-	else {
-		msg->type   = shm_message_queue::msg_new;
-	}
 
 	payload[offset++] = from_len >> 8;
 	payload[offset++] = from_len;
@@ -43,7 +32,67 @@ shm_message_queue::message * wrap_message(const size_t from_len, const uint8_t *
 
 	assert(offset == total_pl_length);
 
+	return { payload, offset };
+}
+
+shm_message_queue::message * wrap_message(const size_t from_len, const uint8_t *const from,
+                                          const size_t to_len,   const uint8_t *const to,
+                                          const size_t pl_len,   const uint8_t *const pl,
+					  const std::optional<uint64_t> reply_to)
+{
+	auto encoded = wrap_message(from_len, from, to_len, to, pl_len, pl);
+	shm_message_queue::message *msg = allocate_shm_message(encoded.second);
+
+	msg->size = encoded.second;
+	if (reply_to.has_value()) {
+		msg->msg_nr = reply_to.value();
+		msg->type   = shm_message_queue::msg_reply;
+	}
+	else {
+		msg->type   = shm_message_queue::msg_new;
+	}
+
+	memcpy(msg->data, encoded.first, msg->size);
+	free(encoded.first);
+
 	return msg;
+}
+
+bool unwrap_message(const std::pair<const uint8_t *, size_t> & in,
+		    size_t *const from_len, const uint8_t **const from,
+                    size_t *const to_len,   const uint8_t **const to,
+                    size_t *const pl_len,   const uint8_t **const pl)
+{
+	const uint8_t *payload = in.first;
+	size_t         offset  = 0;
+
+	if (offset + 2 >= in.second)
+		return false;
+	*from_len = get_uint16(&payload[offset]);
+	offset += 2;
+	if (offset + *from_len >= in.second)
+		return false;
+	*from = &payload[offset];
+	offset += *from_len;
+
+	if (offset + 2 >= in.second)
+		return false;
+	*to_len = get_uint16(&payload[offset]);
+	offset += 2;
+	if (offset + *to_len >= in.second)
+		return false;
+	*to = &payload[offset];
+	offset += *to_len;
+
+	if (offset + 4 >= in.second)
+		return false;
+	*pl_len = get_uint32(&payload[offset]);
+	offset += 4;
+	if (offset + *pl_len > in.second)  // not a typo
+		return false;
+	*pl = &payload[offset];
+
+	return true;
 }
 
 bool unwrap_message(const shm_message_queue::message *const m,
@@ -51,34 +100,8 @@ bool unwrap_message(const shm_message_queue::message *const m,
                     size_t *const to_len,   const uint8_t **const to,
                     size_t *const pl_len,   const uint8_t **const pl)
 {
-	const uint8_t *payload = m->data;
-	size_t         offset  = 0;
-
-	if (offset + 2 >= m->size)
-		return false;
-	*from_len = get_uint16(&payload[offset]);
-	offset += 2;
-	if (offset + *from_len >= m->size)
-		return false;
-	*from = &payload[offset];
-	offset += *from_len;
-
-	if (offset + 2 >= m->size)
-		return false;
-	*to_len = get_uint16(&payload[offset]);
-	offset += 2;
-	if (offset + *to_len >= m->size)
-		return false;
-	*to = &payload[offset];
-	offset += *to_len;
-
-	if (offset + 4 >= m->size)
-		return false;
-	*pl_len = get_uint32(&payload[offset]);
-	offset += 4;
-	if (offset + *pl_len > m->size)  // not a typo
-		return false;
-	*pl = &payload[offset];
-
-	return true;
+	return unwrap_message({ m->data, m->size }, 
+		    from_len, from,
+                    to_len,   to,
+                    pl_len,   pl);
 }
--- src/utils/shm_message.h
+++ src/utils/shm_message.h
@@ -1,12 +1,19 @@
 #include "shm.h"
 
 // de-allocate with free()
+std::pair<uint8_t *, size_t> wrap_message(const size_t from_len, const uint8_t *const from,
+                                          const size_t to_len,   const uint8_t *const to,
+                                          const size_t pl_len,   const uint8_t *const pl);
 shm_message_queue::message * wrap_message(const size_t from_len, const uint8_t *const from,
                                           const size_t to_len,   const uint8_t *const to,
                                           const size_t pl_len,   const uint8_t *const pl,
 					  const std::optional<uint64_t> reply_to);
 
 // will not allocate anything
+bool unwrap_message(const std::pair<const uint8_t *, size_t> & in,
+		    size_t *const from_len, const uint8_t **const from,
+                    size_t *const to_len,   const uint8_t **const to,
+                    size_t *const pl_len,   const uint8_t **const pl);
 bool unwrap_message(const shm_message_queue::message *const m,
 		    size_t *const from_len, const uint8_t **const from,
                     size_t *const to_len,   const uint8_t **const to,
