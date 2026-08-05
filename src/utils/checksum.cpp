49998e3ecbcc54fd75c9d319cfda053976e0b3cb
Folkert van Heusden <folkert@vanheusden.com>
2026-08-06 00:14:24 +0200

    tcp/udp checksumming (with fake-header)

--- src/utils/checksum.cpp
+++ src/utils/checksum.cpp
@@ -1,6 +1,8 @@
 #include <cstdint>
 #include <arpa/inet.h>
 
+#include "addresses.h"
+
 
 uint16_t ip_checksum(const uint16_t *const p, const size_t n)
 {
@@ -11,6 +13,29 @@ uint16_t ip_checksum(const uint16_t *const p, const size_t n)
 
 	cksum = (cksum >> 16) + (cksum & 0xffff);
 	cksum += cksum >> 16;
+
+        return ~cksum;
+}
+
+// TODO optimize me
+uint16_t tcp_udp_checksum(const addr_ip4 & from, const addr_ip4 & to, const uint8_t *const p, const size_t n, const uint8_t protocol_number)
+{
+	uint8_t *temp = new uint8_t[12 + n * 2]();
+
+	memcpy(&temp[0], from.get(), 4);
+	memcpy(&temp[4], to  .get(), 4);
+	temp[9]  = protocol_number;
+	temp[10] = n >> 8;
+	temp[11] = n;
+	memcpy(&temp[12], p, n);
+
+        uint32_t cksum = 0;
+        for(size_t i=0; i<n + 12 / 2; i++) 
+                cksum += htons(reinterpret_cast<const uint16_t *>(temp)[i]);
+	cksum = (cksum >> 16) + (cksum & 0xffff);
+	cksum += cksum >> 16;
+
+	free(temp);
 
         return ~cksum;
 }
--- src/utils/checksum.h
+++ src/utils/checksum.h
@@ -1,5 +1,10 @@
 #include <cstdint>
 
+#include "addresses.h"
+
 
 // n is number of uint16s
 uint16_t ip_checksum(const uint16_t *const p, const size_t n);
+
+// n is number of bytes in p
+uint16_t tcp_udp_checksum(const addr_ip4 & from, const addr_ip4 & to, const uint8_t *const p, const size_t n, const uint8_t protocol_number);
