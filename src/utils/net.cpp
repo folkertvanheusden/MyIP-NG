22e2968b52870ed6ea785c0703a156f69108134a
Folkert van Heusden <folkert@vanheusden.com>
2026-08-06 00:53:22 +0200

    put_uint16

--- src/utils/net.cpp
+++ src/utils/net.cpp
@@ -10,3 +10,9 @@ uint32_t get_uint32(const uint8_t from[4])
 {
 	return (from[0] << 24) | (from[1] << 16) | (from[2] << 8) | from[3];
 }
+
+void put_uint16(uint8_t to[2], const uint16_t from)
+{
+	to[0] = from >> 8;
+	to[1] = from;
+}
