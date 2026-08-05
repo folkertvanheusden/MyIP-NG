c90c4cfaaeec0e8b5655d17bac7acfd13c747748
Folkert van Heusden <folkert@vanheusden.com>
2026-08-05 17:21:42 +0200

    handler added to icmp-server which allow others (e.g. ipv4 server) to send errors on their behalf

--- src/icmp_server.cpp
+++ src/icmp_server.cpp
@@ -108,10 +108,72 @@ void run_in(shm_message_queue *const shm, const std::string & out_name)
 	}
 }
 
-void run(shm_message_queue *const shm, const std::string & out_name)
+void run_err(shm_message_queue *const shm_err, const std::string & out_name, shm_message_queue *const shm_out)
 {
-	std::thread rx ([&] { run_in (shm, out_name); });
-	rx.join();
+	while(!stop_flag) {
+		shm_message_queue::message *m_in = shm_err->wait_for_message(SLEEP_INTERVAL_MS, shm_message_queue::msg_new, { });
+		if (!m_in)
+			continue;
+
+		// unwrap
+		size_t         from_len = 0;
+		size_t         to_len   = 0;
+		size_t         pl_len   = 0;
+		const uint8_t *from     = nullptr;
+		const uint8_t *to       = nullptr;
+		const uint8_t *pl       = nullptr;
+		if (unwrap_message(m_in, &from_len, &from, &to_len, &to, &pl_len, &pl) == false) {
+			DOLOG(logger::ll_error, "Corrupt message in shared memory segment!");
+			free(m_in);
+			continue;
+		}
+
+		if (pl_len < 20) {
+			DOLOG(logger::ll_debug, "ICMP error handler: IP header too short? (%zu)", pl_len);
+			free(m_in);
+			continue;
+		}
+
+		int type = pl[0];
+		int code = pl[1];
+
+		DOLOG(logger::ll_debug, "Creating ICMP packet with type %d and code %d for %s", type, code, addr_ip4(to, to_len).to_str('.', false).c_str());
+
+		size_t ip_header_size    = (pl[2] & 15) * 4;
+		size_t copy_n            = std::min(pl_len - 2, ip_header_size + 64 / 8);
+		size_t icmp_message_size = 8 + copy_n;
+		uint8_t *m_out = new uint8_t[icmp_message_size]();
+		m_out[0] = type;
+		m_out[1] = code;
+		memcpy(&m_out[8], &pl[2], copy_n);
+
+		uint16_t checksum_msg = ip_checksum(reinterpret_cast<const uint16_t *>(m_out), icmp_message_size / 2);
+		m_out[2] = checksum_msg >> 8;
+		m_out[3] = checksum_msg;
+
+		auto *wrapped = wrap_message(
+				from_len,  from,
+				to_len,    to,
+				icmp_message_size, m_out,
+				{ });
+
+		if (shm_out->send_message(out_name, wrapped, false) == false)
+			DOLOG(logger::ll_warning, "Cannot send to %s", out_name.c_str());
+
+		free(wrapped);
+
+		delete [] m_out;
+
+		free(m_in);
+	}
+}
+
+void run(shm_message_queue *const shm, const std::string & out_name, shm_message_queue *const shm_error)
+{
+	std::thread rx ([&] { run_in (shm,       out_name     ); });
+	std::thread err([&] { run_err(shm_error, out_name, shm); });
+	err.join();
+	rx .join();
 }
 
 int main(int argc, char *argv[])
@@ -148,6 +210,11 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "\"out-name\" under \"global\" missing\n");
 		return 1;
 	}
+	std::string error_in_name = iniparser_getstring(d, "global:error-in-name",  "");
+	if (error_in_name.empty()) {
+		fprintf(stderr, "\"error-in-name\" under \"global\" missing\n");
+		return 1;
+	}
 	int msg_queue_size = iniparser_getint(d, "specific:msg-queue-size", 0);
 	if (msg_queue_size == 0) {
 		msg_queue_size = 16384;
@@ -159,11 +226,17 @@ int main(int argc, char *argv[])
 
 	shm_message_queue shm(name, msg_queue_size);
 	if (shm.begin() == false) {
-		fprintf(stderr, "Cannot initialize shared memory segment\n");
+		fprintf(stderr, "Cannot initialize shared memory segment \"%s\"\n", name.c_str());
 		return 1;
 	}
 
-	run(&shm, out_name);
+	shm_message_queue shm_error(error_in_name, msg_queue_size);
+	if (shm_error.begin() == false) {
+		fprintf(stderr, "Cannot initialize shared memory segment \"%s\"\n", error_in_name.c_str());
+		return 1;
+	}
+
+	run(&shm, out_name, &shm_error);
 
 	return 0;
 }
