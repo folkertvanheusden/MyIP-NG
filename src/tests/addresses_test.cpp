5cfd2c436b576f76dedd4acc2d6066ec6a4dd0f2
Folkert van Heusden <folkert@vanheusden.com>
2026-08-06 22:00:29 +0200

    working on addresses-type tests

---
+++ src/tests/addresses_test.cpp
@@ -0,0 +1,29 @@
+#include <cassert>
+#include <map>
+
+#include "../utils/addresses.h"
+
+
+int main(int argc, char *argv[])
+{
+	addr a0("1.2.3.3", ".", false);
+	addr a1("1.2.3.4", ".", false);
+	addr a2("1.2.33.4", ".", true);
+	addr a3("1.2.51.4", ".", false);
+	addr a4("1.2.3.5", ".", false);
+	assert(a1 != a2);
+	assert(a2 == a3);
+
+	std::map<addr, addr, decltype(set_cmp)> m1;
+	m1.insert({ a1, a1 });
+	assert(m1.find(a0) == m1.end());
+	assert(m1.find(a4) == m1.end());
+
+	std::map<addr, addr, decltype(set_cmp)> m2;
+	m2.insert({ a0, a0 });
+	m2.insert({ a1, a1 });
+	assert(m2.find(a0) != m2.end());
+	assert(m2.find(a4) == m2.end());
+
+	return 0;
+}
