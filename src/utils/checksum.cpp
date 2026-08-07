4d4eb1ddbdfcccc3e4c169aace80a4d7ceee19c0
Folkert van Heusden <folkert@vanheusden.com>
2026-08-07 19:26:29 +0200

    corrected checksum & fixed test

--- src/utils/checksum.cpp
+++ src/utils/checksum.cpp
@@ -6,38 +6,37 @@
 
 uint16_t ip_checksum(const uint16_t *const p, const size_t n)
 {
-        uint32_t cksum = 0;
+	uint32_t cksum = 0;
 
-        for(size_t i=0; i<n; i++) 
-                cksum += htons(p[i]);
+	for(size_t i=0; i<n; i++) 
+		cksum += htons(p[i]);
 
 	cksum = (cksum >> 16) + (cksum & 0xffff);
 	cksum += cksum >> 16;
 
-        return ~cksum;
+	return ~cksum;
 }
 
-// TODO optimize me
 uint16_t tcp_udp_checksum(const addr_ip4 & from, const addr_ip4 & to, const uint8_t *const p, const size_t n, const uint8_t protocol_number)
 {
-	uint8_t *temp = new uint8_t[12 + n * 2]();
+	uint32_t cksum = 0;
 
-	memcpy(&temp[0], from.get(), 4);
-	memcpy(&temp[4], to  .get(), 4);
-	temp[9]  = protocol_number;
-	temp[10] = n >> 8;
-	temp[11] = n;
-	memcpy(&temp[12], p, n);
+	const uint16_t *from16 = reinterpret_cast<const uint16_t *>(from.get());
+	cksum += htons(from16[0]);
+	cksum += htons(from16[1]);
+	const uint16_t *to16   = reinterpret_cast<const uint16_t *>(to.get());
+	cksum += htons(to16[0]);
+	cksum += htons(to16[1]);
+	cksum += protocol_number;
+	cksum += n;
 
-        uint32_t cksum = 0;
-        for(size_t i=0; i<n + 12 / 2; i++) 
-                cksum += htons(reinterpret_cast<const uint16_t *>(temp)[i]);
+	for(size_t i=0; i<n; i++)
+		cksum += htons(reinterpret_cast<const uint16_t *>(p)[i]);
+
 	cksum = (cksum >> 16) + (cksum & 0xffff);
 	cksum += cksum >> 16;
 
-	delete [] temp;
-
-        return ~cksum;
+	return ~cksum;
 }
 
 uint64_t fletcher64(const uint8_t *const data, const size_t count)
