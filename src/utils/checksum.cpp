c2d413564bca7146522dbe48907efbfd05d96194
Folkert van Heusden <folkert@vanheusden.com>
2026-08-08 15:05:20 +0200

    OOB fix in fletcher64

--- src/utils/checksum.cpp
+++ src/utils/checksum.cpp
@@ -42,25 +42,25 @@ uint16_t tcp_udp_checksum(const addr_ip4 & from, const addr_ip4 & to, const uint
 
 uint64_t fletcher64(const uint8_t *const data, const size_t count)
 {
-	uint64_t sum1   = 0;
-	uint64_t sum2   = 0;
-	const uint32_t *data32 = reinterpret_cast<const uint32_t*>(data);
-	const size_t    words  = count / 4;
-	for(size_t i=0; i < words; i++) {
-		sum1 = (sum1 + data32[i]) % UINT32_MAX;
-		sum2 = (sum2 + sum1     ) % UINT32_MAX;
-	}
+        uint64_t sum1   = 0;
+        uint64_t sum2   = 0;
+        const uint32_t *data32 = reinterpret_cast<const uint32_t*>(data);
+        const size_t    words  = count / 4;
+        for(size_t i=0; i < words; i++) {
+                sum1 = (sum1 + data32[i]) % UINT32_MAX;
+                sum2 = (sum2 + sum1     ) % UINT32_MAX;
+        }
 
-	int extra = count & 3;
-	if (extra) {
-		size_t   offset = words * 4;
-		uint32_t temp   = 0;
-		for(int i=0; i<count; i++)
-			temp <<= 8, temp |= data[offset + i];
+        int extra = count & 3;
+        if (extra) {
+                size_t   offset = words * 4;
+                uint32_t temp   = 0;
+                for(int i=0; i<extra; i++)
+                        temp <<= 8, temp |= data[offset + i];
 
-		sum1 = (sum1 + temp) % UINT32_MAX;
-		sum2 = (sum2 + sum1) % UINT32_MAX;
-	}
+                sum1 = (sum1 + temp) % UINT32_MAX;
+                sum2 = (sum2 + sum1) % UINT32_MAX;
+        }
 
-	return (sum2 << 32) | sum1;
+        return (sum2 << 32) | sum1;
 }
