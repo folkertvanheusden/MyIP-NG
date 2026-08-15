8491ff921d0e88de3a22ce1c3deda13a6a4e7eb6
Folkert van Heusden <folkert@vanheusden.com>
2026-08-15 23:11:02 +0200

    my_stoull_hex

--- src/utils/stoi.h
+++ src/utils/stoi.h
@@ -1,6 +1,8 @@
+#include <cstdint>
 #include <optional>
 #include <string>
 
 
 std::optional<int> my_stoi_dec(const std::string & str);
 std::optional<int> my_stoi_hex(const std::string & str);
+std::optional<uint64_t> my_stoull_hex(const std::string & str);
