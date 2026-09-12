2670ffcc05fda413419a87010bbba002e8097a13
Folkert van Heusden <folkert@vanheusden.com>
2026-09-12 21:44:38 +0200

    tcp-follower.py: states / payload-len fix / output tweaks

--- tools/tcp-follower.py
+++ tools/tcp-follower.py
@@ -11,6 +11,12 @@ class seq_nrs:
     start_local = 0
     local_calc = 0
 
+class state:
+    wait_for_syn = 0
+    syn_received = 1
+    syn_ack_sent = 2
+    established = 3
+
 srvr_port = None
 
 client = seq_nrs()
@@ -18,17 +24,41 @@ server = seq_nrs()
 
 nr = 0
 
+s = state.wait_for_syn
+
 for p in packets:
     if IP not in p or TCP not in p:
         continue
 
+    err = ''
+
+    ip = p[IP]
     tcp = p[TCP]
-    payload_len = len(tcp.payload)
+    payload_len = ip.len - tcp.dataofs * 4 - ip.ihl * 4
     if tcp.flags == 'S':
-        client.local_calc = client.start_local = client.local = tcp.seq
-        srvr_port = tcp.dport
+        if s == state.wait_for_syn:
+            client.local_calc = client.start_local = client.local = tcp.seq
+            srvr_port = tcp.dport
+            s = state.syn_received
+        else:
+            err = '!'
     elif tcp.flags == 'SA':
+        if s == state.syn_received:
+            s = state.syn_ack_sent
+        else:
+            err = '!'
         server.local_calc = server.start_local = server.local = tcp.seq
+    elif tcp.flags == 'A':
+        if s == state.syn_ack_sent:
+            s = state.established
+
+    seq_rel_offset = server.start_local if tcp.sport == srvr_port else client.start_local
+    ack_rel_offset = client.start_local if tcp.sport == srvr_port else server.start_local
+    exp_seq = client.local_calc if tcp.dport == srvr_port else server.local_calc
+    exp_ack = client.local_calc if tcp.sport == srvr_port else server.local_calc
+
+    if tcp.seq != exp_seq or tcp.ack != exp_ack:
+        err = '!'
 
     nr += 1
     ts_str = time.strftime('%H:%M:%S', time.gmtime(float(p.time))) + f'.{int(p.time * 1000000) % 1000000:06}'
@@ -37,12 +67,12 @@ for p in packets:
         f"{ts_str} "
         f"{tcp.sport:5} -> {tcp.dport:5} "
         f"flags={tcp.sprintf('%TCP.flags%'):8} "
-        f"seq={tcp.seq - (server.start_local if tcp.sport == srvr_port else client.start_local):10} "
-        f"ack={tcp.ack - (client.start_local if tcp.sport == srvr_port else server.start_local):10} "
-        f"expseq={(client.local_calc if tcp.dport == srvr_port else server.local_calc) - (server.start_local if tcp.sport == srvr_port else client.start_local):10} "
-        f"expack={(client.local_calc if tcp.sport == srvr_port else server.local_calc) - (server.start_local if tcp.dport == srvr_port else client.start_local):10} "
+        f"seq={tcp.seq - seq_rel_offset:10} "
+        f"expseq={exp_seq - seq_rel_offset:10} "
+        f"ack={tcp.ack - ack_rel_offset:10} "
+        f"expack={exp_ack - ack_rel_offset:10} "
         f"len={payload_len:5} "
-        f"win={tcp.window}")
+        f"win={tcp.window} {err}")
 
     if 'S' in tcp.flags or 'F' in tcp.flags:
         if tcp.sport == srvr_port:
